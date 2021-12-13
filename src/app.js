70742df2fc8665371d77b15122025c57a51caa29
nggill <92691516+nggill@users.noreply.github.com>
2021-12-13 12:37:39 -0600

    Changed code to be able to search API and call correct data for city that is search by user.

--- src/app.js
+++ src/app.js
@@ -29,8 +29,18 @@ dateElement.innerHTML=formatDate(response.data.dt *1000);
 iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt",response.data.weather[0].description);
 }
-let apiKey="23ae8af4580cf1669a4ba3e951eb0962";
-let city= "Paris";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city){
 
-axios.get(apiUrl).then(displayTemperature);
+    let apiKey="23ae8af4580cf1669a4ba3e951eb0962";
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event){event.preventDefault();
+let cityInputElement=document.querySelector("#city-input");
+search(cityInputElement.value);
+}
+search("New York");
+
+let form= document.querySelector("#search-form");
+form.addEventListener("submit",handleSubmit);
