9d8793cecd51fc0e0c588c0ba5d3838f9a789616
nggill <92691516+nggill@users.noreply.github.com>
2021-12-13 10:28:04 -0600

    Showing the current temperature.

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ function displayTemperature(response){
 let temperatureElement=document.querySelector("#temperature");
 let cityElement=document.querySelector("#city");
 let descriptionElement=document.querySelector("#description");
+let humidityElement=document.querySelector("#humidity");
+let windElement=document.querySelector("#wind");
 temperatureElement.innerHTML=Math.round(response.data.main.temp);
 cityElement.innerHTML=response.data.name;
-descriptionElement.innerHTML=response.data.weather[0].description;}
+descriptionElement.innerHTML=response.data.weather[0].description;
+humidityElement.innerHTML=response.data.main.humidity;
+windElement.innerHTML=Math.round(response.data.wind.speed);
+}
 let apiKey="23ae8af4580cf1669a4ba3e951eb0962";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
