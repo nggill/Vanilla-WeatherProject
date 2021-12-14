6e3e6031df2f1076bf66c308d432ce4634c48fb6
nggill <92691516+nggill@users.noreply.github.com>
2021-12-14 11:14:21 -0600

    Updated link of C/F to create better UI experience.

--- src/app.js
+++ src/app.js
@@ -20,6 +20,9 @@ let humidityElement=document.querySelector("#humidity");
 let windElement=document.querySelector("#wind");
 let dateElement=document.querySelector("#date");
 let iconElement=document.querySelector("#icon");
+
+celciusTemperature= response.data.main.temp;
+
 temperatureElement.innerHTML=Math.round(response.data.main.temp);
 cityElement.innerHTML=response.data.name;
 descriptionElement.innerHTML=response.data.weather[0].description;
@@ -42,12 +45,26 @@ search(cityInputElement.value);
 }
 function displayFahrenheitTemperature(event){
     event.preventDefault();
-    alert("Link clicked");
+    let fahrenheitTemperature=(celsiusTemperature*9)/5+32;
+    let temperatureElement=document.querySelector("#temperature");
+    temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
 }
-search("New York");
+
+function displayCelsiusTemperature(event){
+    event.preventDefault();
+    let temperatureElement=document.querySelector("#temperature");
+    temperatureElement.innerHTML= Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature=null;
 
 let form= document.querySelector("#search-form");
 form.addEventListener("submit",handleSubmit);
 
 let fahrenheitLink=document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink=document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("New York");
